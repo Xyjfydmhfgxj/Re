ad44c08f1a3cc4ae1f7332fa3391216a0084e075
Xyjfydmhfgxj <mrsydone@gmail.com>
2026-08-06 17:25:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ class Bot(Client):
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
             workers=200,
-            plugins={"root": "plugins"},
+            plugins={"root": "For"},
             sleep_threshold=15,
         )
 
