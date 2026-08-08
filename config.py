a59973ba575902c35095175d842b6d13a9c1cf36
Xyjfydmhfgxj <mrsydone@gmail.com>
2026-08-08 16:58:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class Config(object):
         admin) else admin for admin in os.environ.get('ADMIN', '').split()]  # ⚠️ Required
 
    # FORCE_SUB = os.environ.get("FORCE_SUB", "") # ⚠️ Required Username without @
-  #  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))  # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))  # ⚠️ Required
     FLOOD = int(os.environ.get("FLOOD", '10'))
     BANNED_USERS = set(int(x) for x in os.environ.get(
         "BANNED_USERS", "1234567890").split())
